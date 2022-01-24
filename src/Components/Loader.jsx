9e3f65e30c6b6c8417a245c26b0d7aa5061784ba
alceil <ashishshajisrampickal@gmail.com>
2022-01-24 12:14:55 +0530

    added loader component

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,8 @@
+export const Loader = () => {
+    return (
+      <svg
+        className="animate-spin h-8 w-8 rounded-full border-2 border-transparent border-opacity-50 loader-border"
+        viewBox="0 0 24 24"
+      ></svg>
+    );
+  };
